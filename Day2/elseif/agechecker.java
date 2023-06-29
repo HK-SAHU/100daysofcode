6860ce00077faf7a6db00365af21464e5b4ffbd7
HK-SAHU <135067768+HK-SAHU@users.noreply.github.com>
2023-06-30 00:22:38 +0530

    Add files via upload
    DAY 2

---
+++ Day2/elseif/agechecker.java
@@ -0,0 +1,19 @@
+package elseif;
+import java.util.Scanner;
+
+public class agechecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Age:");
+        int age =sc.nextInt();
+        if(age>=18 && age<=60){
+            System.out.println("you can drive");
+        }
+        else if(age<18){
+            System.out.println("you can't drive");
+        }
+        else{
+            System.out.println("you are senior citizen");
+        }
+    }
+}
